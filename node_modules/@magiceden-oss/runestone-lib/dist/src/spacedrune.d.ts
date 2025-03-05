244c239e062b7b893102d4fba1ce484bc88afeb9
furo-tests-in-prod <dealdealdomains@gmail.com>
2025-03-05 17:53:43 -0500

    delete first version

--- node_modules/@magiceden-oss/runestone-lib/dist/src/spacedrune.d.ts
+++
@@ -1,9 +0,0 @@
-import { Rune } from './rune';
-export declare class SpacedRune {
-    readonly rune: Rune;
-    readonly spacers: number;
-    constructor(rune: Rune, spacers: number);
-    static fromString(s: string): SpacedRune;
-    toString(): string;
-}
-//# sourceMappingURL=spacedrune.d.ts.map
